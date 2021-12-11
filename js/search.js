463f0046b062f2207683b75b2629a1d62de9c5a4
54aaron <arj2145@columbia.edu>
2021-12-11 13:43:41 -0500

    apply saved filters prototyped - vvvvvvv light javascript used :)

---
+++ js/search.js
@@ -0,0 +1,15 @@
+function change(){
+
+    setTimeout(function(){
+        window.location.href = '../views/search_none.html';
+     }, 500);
+
+}
+
+function change_back(){
+
+    setTimeout(function(){
+        window.location.href = '../views/search.html';
+     }, 500);
+
+}
