ef1e57ec9075a0876dee226b2917adaaafbae2fa
54aaron <arj2145@columbia.edu>
2021-12-13 23:13:08 -0500

    added a lot of stuff

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,4 @@
-function change(){
+function searchNone(){
 
     setTimeout(function(){
         window.location.href = '../views/search_none.html';
@@ -6,10 +6,16 @@ function change(){
 
 }
 
-function change_back(){
+function search(){
 
     setTimeout(function(){
         window.location.href = '../views/search.html';
      }, 500);
 
+}
+
+function uploadNew(){
+    setTimeout(function(){
+        window.location.href = '../views/upload_new.html';
+     }, 500);
 }
