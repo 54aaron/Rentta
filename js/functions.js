1509d72bb8ec23e888584c341c83993f00631d07
54aaron <arj2145@columbia.edu>
2021-12-14 02:21:02 -0500

    going 2 sleep

--- js/functions.js
+++ js/functions.js
@@ -19,3 +19,14 @@ function uploadNew(){
         window.location.href = '../views/upload_new.html';
      }, 500);
 }
+
+function getInputValue(){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("password").value;
+    
+    if(inputVal == "password"){
+        window.location.href = '../views/upload.html';
+    }else{
+        alert("Incorrect Password");
+    }
+}
